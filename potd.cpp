1c50b243af152c10f9c8d4a8dfa2487b4301df0f
PratikThakurDev <149695885+PratikThakurDev@users.noreply.github.com>
2025-09-17 18:22:28 +0000

    did the 'make a food rating system ' problem in lc

--- potd.cpp
+++ potd.cpp
@@ -21,6 +21,34 @@ vector<int> replaceNonCoprimes(vector<int>& nums) {
     return ans ;
 }
 
+class FoodRatings {
+public : 
+    unordered_map<string , set<pair<int, string>>> cuisineToFoods ;
+    unordered_map<string , pair<int, string >> foodInfo ;
+
+    FoodRatings ( vector<string> & foods , vector<string>& cuisines, vector<int> & ratings ) {
+        for (int i = 0 ; i < foods.size() ; i++ ) {
+            string food = foods[i] ;
+            string cuisine = cuisines[i] ;
+            int rating = ratings[i] ;
+            cuisineToFoods[cuisine].insert( {-rating , food }) ;
+            foodInfo[food] = {rating , cuisine } ;
+        }
+    }
+
+    void changeRating( string food , int newRating) {
+        auto [oldRating , cuisine ] = foodInfo[food] ;
+        cuisineToFoods[cuisine].erase({-oldRating , food }) ;
+        cuisineToFoods[cuisine].insert( {-newRating , food }) ;
+        foodInfo[food] = {newRating , cuisine } ;
+    }
+
+    string highestRated( string cuisine ) {
+        return cuisineToFoods[cuisine].begin()->second ;
+    }
+
+} ;
+
 int main ( ) {
 
 return 0;
