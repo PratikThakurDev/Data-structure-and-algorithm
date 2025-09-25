1e52c0e8ef2d499878bab604433e2f0f115fd0b7
PratikThakurDev <149695885+PratikThakurDev@users.noreply.github.com>
2025-09-25 14:37:02 +0000

    did the minimum total in a triangle problem

--- potd.cpp
+++ potd.cpp
@@ -333,6 +333,18 @@ string fractionToDecimal(int numerator, int denominator) {
     return ans + frac;
 }
 
+int minimumTotal(vector<vector<int>>& triangle) {
+    int n = triangle.size();
+    vector<int> dp(triangle.back()); // Initialize dp with last row elements
+
+    for (int row = n - 2; row >= 0; row--) {
+        for (int col = 0; col <= row; col++) {
+            dp[col] = triangle[row][col] + min(dp[col], dp[col + 1]);
+        }
+    }
+    return dp[0];
+}
+
 int main ( ) {
 
 return 0;
