cc0cd2084a35a475e6e15a6f1560058bc26418f5
PratikThakurDev <149695885+PratikThakurDev@users.noreply.github.com>
2025-09-27 14:09:12 +0000

    did the checkIthbit problem

---
+++ bit_manipulation/easy.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+using namespace std ;
+
+// bool checkIthBit(int n, int i) {
+//     string binaryForm ;
+//     while ( n > 0 ) {
+//         binaryForm += n % 2 == 0 ? 0 : 1 ;
+//         n = n / 2 ;
+//     }
+//     if ( binaryForm.size() > i) return false ;
+
+//     return binaryForm[i] == '1' ;
+// }
+
+bool checkIthBit ( int n , int i ) {
+     
+    return ( (n & ( 1 << i))!= '0' ) ;
+}
