1f45760fb181d72750255436a78e9220e695d4e8
PratikThakurDev <149695885+PratikThakurDev@users.noreply.github.com>
2025-09-30 13:12:56 +0000

    did the find the two numbers appearing odd number of times

--- bit_manipulation/easy.cpp
+++ bit_manipulation/easy.cpp
@@ -125,6 +125,27 @@ int countPrimes(int n) {
     return cnt ;
 }
 
+vector<int> singleNumber(vector<int>& nums){
+    int n = nums.size();
+    long XOR = 0;
+    for(int i=0; i < n; i++) {
+        XOR = XOR ^ nums[i];
+    }
+    int rightmost = (XOR & (XOR - 1)) ^ XOR;
+    int XOR1 = 0, XOR2 = 0;
+
+    for(int i=0; i < n; i++) {
+        if( nums[i] & rightmost == 1 ) {
+            XOR1 = XOR1 ^ nums[i];
+        }
+        else {
+            XOR2 = XOR2 ^ nums[i];
+        }
+    }
+    if(XOR1 < XOR2) return {XOR1, XOR2};
+    return {XOR2, XOR1};
+}
+
 int main() {
 
 return 0 ;
