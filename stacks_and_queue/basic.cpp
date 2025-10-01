489e3f03df6ad552871d0ceae47c08f86066d79e
PratikThakurDev <149695885+PratikThakurDev@users.noreply.github.com>
2025-10-01 14:38:49 +0000

    implemented stack using queue

--- stacks_and_queue/basic.cpp
+++ stacks_and_queue/basic.cpp
@@ -39,7 +39,7 @@ class QueueUsingArray{
     int * arr ;
     int currSize ;
     public :
-    
+
     QueueUsingArray() {
         int Size = 1000 ;
         arr = new int[maxSize] ;
@@ -92,7 +92,37 @@ class QueueUsingArray{
     }
 } ;
 
-
+class MyStack {
+public:
+    queue <int> q ;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        int s = q.size() ;
+        q.push(x) ;
+        for ( int i = 0 ; i < s ; i++ ) {
+            q.push( q.front()) ;
+            q.pop() ;
+        }
+    }
+    
+    int pop() {
+        int popped = q.front() ;
+        q.pop() ;
+        return popped ;
+    }
+    
+    int top() {
+        return q.front() ;
+    }
+    
+    bool empty() {
+        if ( q.size() == 0 ) return true ;
+        return false ;
+    }
+};
 
 
 int main ( ) {
