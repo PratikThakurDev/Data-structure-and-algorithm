0f9c10007296d3ae024560bc3707c6d87dc131a5
PratikThakurDev <149695885+PratikThakurDev@users.noreply.github.com>
2025-10-03 11:33:12 +0000

    did the next greatest integer 2 problem

--- stacks_and_queue/monotonic_stack.cpp
+++ stacks_and_queue/monotonic_stack.cpp
@@ -19,3 +19,31 @@ vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
     }
     return result;
 }
+
+vector<int> nextGreaterElements(vector<int>& nums) {
+    int n = nums.size() ;
+    stack<int> st ;
+    vector<int> nge ;
+    for ( int i = 2*n - 1 ; i >=0 ; i-- ) {
+        while ( !st.empty() && nums[i % n] >= st.top() ) st.pop() ;
+        if ( i < n ) {
+            nge[i] = st.top() ;
+        }
+        st.push( nums[i%n] ) ;
+    }
+    return nge ;
+}
+
+vector<int> nextGreaterElementsII(vector<int>& nums) {
+    int n = nums.size() ;
+    stack<int> st ;
+    vector<int> nge(n) ;
+    for ( int i = 2*n - 1 ; i >=0 ; i-- ) {
+        while ( !st.empty() && nums[i % n] >= st.top() ) st.pop() ;
+        if ( i < n ) {
+            nge[i] = st.empty() ? -1 : st.top() ;
+        }
+        st.push( nums[i%n] ) ;
+    }
+    return nge ;
+}
