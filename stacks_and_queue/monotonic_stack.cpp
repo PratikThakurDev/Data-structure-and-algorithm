02df12a015001aa1fe1569b247d15fa7cd204feb
PratikThakurDev <149695885+PratikThakurDev@users.noreply.github.com>
2025-10-04 08:37:10 +0000

    did the rainwater problem

--- stacks_and_queue/monotonic_stack.cpp
+++ stacks_and_queue/monotonic_stack.cpp
@@ -4,7 +4,7 @@ using namespace std ;
 vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
     stack <int> st ;
     int n = nums2.size() ;
-    unordered_map<int,int> nge ( n , -1);
+    unordered_map<int,int> nge ;
     for ( int i = n - 1 ; i >=0 ; i--) {
         while ( !st.empty() && st.top() <= nums2[i % n]) {
             st.pop() ;
@@ -18,20 +18,6 @@ vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         result.push_back(nge[it]) ;
     }
     return result;
-}
-
-vector<int> nextGreaterElements(vector<int>& nums) {
-    int n = nums.size() ;
-    stack<int> st ;
-    vector<int> nge ;
-    for ( int i = 2*n - 1 ; i >=0 ; i-- ) {
-        while ( !st.empty() && nums[i % n] >= st.top() ) st.pop() ;
-        if ( i < n ) {
-            nge[i] = st.top() ;
-        }
-        st.push( nums[i%n] ) ;
-    }
-    return nge ;
 }
 
 vector<int> nextGreaterElementsII(vector<int>& nums) {
@@ -58,4 +44,25 @@ vector<int> nextSmallerElements(vector<int>& nums) {
         st.push( nums[i] ) ;
     }
     return nse ;
+}
+
+int trap(vector<int>& height) {
+    int n = height.size() ;
+    int lMax = 0 , rMax = 0 , total = 0 ;
+    int l = 0 ;
+    int r = n - 1 ;
+    while ( l < r ) {
+        if ( height[l] <= height[r] ) {
+            if ( lMax > height[l] ) {
+                total += lMax - height[l] ;
+            } else lMax = height[l] ;
+            l++ ;   
+        } else {
+            if ( rMax > height[r] ) {
+                total += rMax - height[r] ;
+            } else rMax = height[r] ;
+            r-- ;
+        }
+    }
+    return total ;
 }
