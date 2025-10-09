d9a5101388756e0037e993baeeec89c54b9a618d
PratikThakurDev <149695885+PratikThakurDev@users.noreply.github.com>
2025-10-09 15:56:11 +0000

    did the max Sliding window problem

--- stacks_and_queue/monotonic_stack.cpp
+++ stacks_and_queue/monotonic_stack.cpp
@@ -274,6 +274,19 @@ int maximalRectangle(vector<vector<char>>& matrix) {
     return maxArea;
 }
 
+vector<int> maxSlidingWindow ( vector<int> & nums , int k ) {
+    int n = nums.size() ;
+    deque<int> dq ;
+    vector<int> ans ;
+    for ( int i = 0 ; i < n ; i++ ){
+        if ( !dq.empty() && dq.front() == i - k ) dq.pop_front() ;
+        while ( !dq.empty() && nums[dq.back()] < nums[i] ) dq.pop_back() ;
+        dq.push_back(i) ;
+        if ( i > k - 2 ) ans.push_back(nums[dq.front()]) ;
+    }
+    return ans ;
+}
+
 int main () {
 
 return 0 ;
